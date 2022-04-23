4444c2d8cb4cf3217819e276b6bd797f4cd497d3
MARCOS TUROSSI <04853773908@edu.udesc.br>
2022-04-23 18:48:32 -0300

    doc: colocando docstring nos testes

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -7,11 +7,12 @@ client = TestClient(app)
 
 
 def test():
+    """This teste verirfy if the book create end-point is working"""
     response = client.post('http://127.0.0.1:8000/book/', data=json.dumps({'nome': 'Game of Thrones'}))
     print(json.loads(response.text))
 
 
 def test_id():
+    """This teste verify if the get book by id is working"""
     response = client.get('http://127.0.0.1:8000/book/1/')
     assert response.status_code == 200
-
